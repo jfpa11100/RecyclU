02cfb199102d9cc137f3e465a7044d977e61bb7f
jfpa11100 <felipepala1000@gmail.com>
2023-09-30 20:22:38 -0500

    Modelos y migracion

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecyclU.Data;
+using RecyclU.Models;
+
+namespace RecyclU.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly RecyclUContext _context;
+
+        public PostsController(RecyclUContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index()
+        {
+              return _context.Post != null ? 
+                          View(await _context.Post.ToListAsync()) :
+                          Problem("Entity set 'RecyclUContext.Post'  is null.");
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Imagen,Descripcion,Precio,Material,Peso")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Imagen,Descripcion,Precio,Material,Peso")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Post == null)
+            {
+                return Problem("Entity set 'RecyclUContext.Post'  is null.");
+            }
+            var post = await _context.Post.FindAsync(id);
+            if (post != null)
+            {
+                _context.Post.Remove(post);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+          return (_context.Post?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/RecyclUContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecyclU.Models;
+
+namespace RecyclU.Data
+{
+    public class RecyclUContext : DbContext
+    {
+        public RecyclUContext (DbContextOptions<RecyclUContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecyclU.Models.Usuario> Usuario { get; set; } = default!;
+
+        public DbSet<RecyclU.Models.Empresa> Empresa { get; set; } = default!;
+
+        public DbSet<RecyclU.Models.Universidad> Universidad { get; set; } = default!;
+
+        public DbSet<RecyclU.Models.Negocio> Negocio { get; set; } = default!;
+
+        public DbSet<RecyclU.Models.Post> Post { get; set; } = default!;
+    }
+}
